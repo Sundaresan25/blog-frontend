51fae5c65c6eb2c8b3e4cc7348fc0557c65eece7
sundar <sundarbgm98@gmail.com>
2023-06-04 11:38:46 +0530

    read me added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import { lazy } from "react";
 import { Suspense } from "react";
 import { Loader } from "./Components/Global/Loader";
 
+// Lazy load the main index component
 const LazyIndex = lazy(() => import("./Pages"));
 
 function App() {
   return (
     <div className="App">
+      {/* Toast notifications */}
       <Toaster position="top-right" reverseOrder={false} />
+
+      {/* Suspense component for lazy loading */}
       <Suspense fallback={<Loader />}>
+        {/* Lazy loaded index component */}
         <LazyIndex />
       </Suspense>
     </div>
